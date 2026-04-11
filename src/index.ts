3e3fd06ad608837b546e08eb40b57995a125fa41
ndivanov <iwaa0303@mail.ru>
2026-04-11 19:05:29 +0300

    Prepare package build for external consumption

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export * from "./clock/clock";
+
+export * from "./crdt/array";
+export * from "./crdt/object";
+export * from "./crdt/register";
+export * from "./crdt/set";
+export * from "./crdt/state";
+export * from "./crdt/version";
+
+export * from "./ops/action";
+export * from "./ops/operation";
+export * from "./ops/transaction";
+
+export * from "./runtime/apply";
+export * from "./runtime/leafUtils";
+export * from "./runtime/materializer";
+export * from "./runtime/pathHelpers";
+export * from "./runtime/replica";
+export * from "./runtime/view";
